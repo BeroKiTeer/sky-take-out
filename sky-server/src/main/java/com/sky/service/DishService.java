dba6bc75e6911d658d1853444e4ca1d79ccee7f8
BeroKiTeer <1377433202@qq.com>
2024-03-02 23:00:13 +0800

    修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -3,6 +3,7 @@ package com.sky.service;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 
 import java.util.List;
 
@@ -18,11 +19,24 @@ public interface DishService {
      * @param dishPageQueryDTO
      * @return
      */
-    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 
     /**
      * 菜品的批量删除
      * @param ids
      */
-    void deleteBatch(List<Long> ids);
+    public void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询菜品和对应的口味数据
+     * @param id
+     * @return
+     */
+    public DishVO getByIdWithFlavor(Long id);
+
+    /**
+     * 根据ID修改菜品基本信息和口味信息
+     * @param dishDTO
+     */
+    public void updateWithFlavor(DishDTO dishDTO);
 }
