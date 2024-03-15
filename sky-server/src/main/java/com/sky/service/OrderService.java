682e23dc18ae8a2f0cd4c605ad8b929f0b31f9e0
BeroKiTeer <1377433202@qq.com>
2024-03-15 19:17:01 +0800

    再来一单

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -47,4 +47,9 @@ public interface OrderService {
      * @param id
      */
     void userCancelById(Long id) throws Exception;
+    /**
+     * 再来一单
+     * @param id
+     */
+    void repetition(Long id);
 }
