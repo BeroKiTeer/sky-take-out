78277c81720475f8d4ce16b0500532df5b635e46
BeroKiTeer <1377433202@qq.com>
2024-03-16 21:43:54 +0800

    来单提醒

--- sky-server/src/main/java/com/sky/task/MyTask.java
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -9,7 +9,7 @@ import java.util.Date;
 @Component
 @Slf4j
 public class MyTask {
-    @Scheduled(cron = "0/5 * * * * ?")
+    @Scheduled(cron = "0/10 * * * * ?")
     public void executeTask() {
         log.info("定时任务开始执行:{}",new Date());
     }
